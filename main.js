2cbb6286734ca86a892fadfcc98eb05561c365d8
Bramtej <79318078+Bramtej@users.noreply.github.com>
2021-10-11 20:43:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,104 @@
+function gototelugubooks()
+{
+    window.location="telugu books.html";
+}
+
+function gotoenglishbooks()
+{
+    window.location = "english books.html";
+}
+
+function readteluguhanumanbook()
+{
+    window.location = "telugu hanuman book.html";
+}
+
+function readteluguNaseeruddinbook()
+{
+    window.location = "telugu nasseruddin book.html";
+}
+
+function readenglishhanumanbook()
+{
+    window.location = "english hanuman book.html";
+}
+
+function readenglishnaseeruddinbook()
+{
+    window.location = "english naseeruddin book.html";
+}
+
+function readenglishjatakabook()
+{
+    window.location = "english jataka book.html";
+}
+
+function readtelugujatkabook()
+{
+    window.location = "telugu jatakabook.html";
+}
+
+function back()
+{
+    window.location = "INDEX.HTML"
+}
+
+function eback1()
+{
+    window.location ="english books.html";
+}
+
+function eback2()
+{
+    window.location ="english books.html";
+}
+
+function eback3()
+{
+    window.location ="english books.html";
+}
+
+function tback1()
+{
+    window.location = "telugu books.html";
+}
+
+function tback2()
+{
+    window.location = "telugu books.html";
+}
+
+function tback3()
+{
+    window.location = "telugu books.html";
+}
+
+function readtelugualibaba()
+{
+    window.location = "telugu alibaba.html";
+}
+
+function readteluguAladdin()
+{
+    window.location = "telugu aladdin.html";
+}
+
+function readtelugukrishna()
+{
+    window.location = "telugu krishna.html";
+}
+
+function readenglishalibababook()
+{
+    window.location = "english alibaba.html";
+}
+
+function readenglishkrishnabook()
+{
+    window.location = "english krishna.html";
+}
+
+function readenglishaladdinbook()
+{
+    window.location = "english aladdin.html";
+}
